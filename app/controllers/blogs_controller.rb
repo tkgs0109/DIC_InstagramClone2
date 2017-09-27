8559001c75b4df10cd0880d50244a95d20f5b798
tkgs0109 <subtkgs@gmail.com>
2017-09-27 08:55:22 +0000

    push test to github

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,8 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @blogs = Blog.all
   end
   
   def new
@@ -7,7 +10,39 @@ class BlogsController < ApplicationController
   end
   
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
-    redirect_to new_blog_path
+    @blog = Blog.create(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました!"
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました!"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "ブログを削除しました!"
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
