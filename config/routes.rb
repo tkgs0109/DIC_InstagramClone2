f9d03228e5a6989e14333d2d47062407d90fb3a2
tkgs0109 <subtkgs@gmail.com>
2017-10-13 09:45:15 +0000

    シリーズ課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'blogs#index'
   resources :blogs
+  resources :contacts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922071021) do
+ActiveRecord::Schema.define(version: 20170929085307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
+    t.text "content"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.text "content"
   end
 
