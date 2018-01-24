00781b6fe927b01afc7a591c3d4334b2d69e4e02
tkgs0109 <subtkgs@gmail.com>
2018-01-24 11:50:35 +0000

    edit icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
   def new
     @user = User.new
   end
@@ -13,12 +15,24 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @favorite_blogs = @user.favorite_blogs
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      @favorite_blogs = @user.favorite_blogs
+      render 'show'
+    end
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :icon)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
